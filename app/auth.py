828014f872fbfc921c40c5faa32d3bec3c4f8c0a
gajawada yashwanth <gajawadayashwanth14@gmail.com>
2025-02-21 11:28:02 +0530

    updated the authentication file

--- app/auth.py
+++ app/auth.py
@@ -36,7 +36,7 @@ def create_access_token(data:dict,expires_delta:timedelta|None =None):
     return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
 #decode a Jwt token and validate it
-def get_current_user(token:str=Depends(oauth2_scheme),db:Session==Depends(get_db)):
+def get_current_user(token:str=Depends(oauth2_scheme), db : Session==Depends(get_db)): # type: ignore
     credentials_exception=HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
